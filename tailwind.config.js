17e601ffc53f9aa96566c9dd25744063bf4ddc12
Alan Lima <dev.alanlima@gmail.com>
2023-10-09 14:23:18 -0300

    feat: Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
         "./app.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                color_primary: 'rgb(var(--color-primary) / <alpha-value>)',
+                color_secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+                color_terciary: 'rgb(var(--color-terciary) / <alpha-value>)',
+                color_dark: 'rgb(var(--color-dark) / <alpha-value>)',
+                color_light: 'rgb(var(--color-light) / <alpha-value>)',
+                color_success: 'rgb(var(--color-success) / <alpha-value>)',
+                color_danger: 'rgb(var(--color-danger) / <alpha-value>)',
+                color_neutral: 'rgb(var(--color-neutral) / <alpha-value>)',
+            }
+        },
     },
     plugins: [],
 }
