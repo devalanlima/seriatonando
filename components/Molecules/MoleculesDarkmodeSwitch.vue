54cbd9f403da47f966bc6c11a24d11c280adec4a
Alan Lima <dev.alanlima@gmail.com>
2023-10-14 20:55:02 -0300

    feat: Create dark mode switch component

---
+++ components/Molecules/MoleculesDarkmodeSwitch.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex gap-2">
+    <AtomsIconsSun
+      :size="20"
+      :color="colorMode.value === 'light'? 'fill-color_primary': 'fill-color_light'"
+    />
+    <AtomsSwitchButton
+      v-model:value="atualTheme"
+    />
+    <AtomsIconsMoon
+      :size="20"
+      :color="colorMode.value === 'dark'? 'fill-color_primary': 'fill-color_light'"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+const themeCookie = useCookie("theme")
+
+const atualTheme = ref<boolean>(colorMode.value === 'dark'? true: false)
+
+watch(()=>atualTheme.value, (newValue)=>{
+  if (newValue) {
+    colorMode.value = 'dark'
+    themeCookie.value = 'dark'
+  } else {
+    colorMode.value = 'light'
+    themeCookie.value = 'light'
+  }
+})
+
+onMounted(()=>{
+  console.log(atualTheme.value);
+  
+})
+</script>
+
+<style scoped></style>
