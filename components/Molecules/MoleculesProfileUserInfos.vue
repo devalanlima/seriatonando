0a3c1b86b2bf4f9c1fc46588b52a25c81935a2d5
Alan Lima <dev.alanlima@gmail.com>
2023-10-14 22:08:25 -0300

    feat: Create profile user infos component

---
+++ components/Molecules/MoleculesProfileUserInfos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex gap-5 justify-between">
+    <AtomsIconsAvatar
+      color="fill-color_primary"
+      :size="50"
+    />
+    <div class="flex flex-col justify-between">
+      <p class="text-xl">{{ props.name }}</p>
+      <p class="text-color_secondary text-sm">{{ props.email }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string;
+  email: string;
+  avatarUrl?: string;
+}
+
+const props = defineProps<Props>()
+</script>
