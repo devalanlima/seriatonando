cc94904f14fa29e58be205dfc9aa99a18af40969
Alan Lima <dev.alanlima@gmail.com>
2023-10-14 22:09:52 -0300

    feat: Create menu card component

---
+++ components/Organisms/OrganismsMenuCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <AtomsBaseCard class="flex w-fit flex-col gap-2">
+    <MoleculesProfileUserInfos
+      :name="userInfos.name"
+      :email="userInfos.email"
+    />
+    <MoleculesMenuOptions
+      class="-mx-6"
+      :options="menuItems"
+      v-model:output="menuItemClicked"
+    >
+      <template #lastOption>
+        <MoleculesDarkmodeSwitch class="px-6" />
+      </template>
+    </MoleculesMenuOptions>
+  </AtomsBaseCard>
+</template>
+
+<script setup lang="ts">
+const menuItems = ref(["Account", "Settings", "Log out"])
+
+const userInfos = ref({
+  name: 'Alan Lima',
+  email: 'example@mail.com',
+})
+
+const menuItemClicked = ref<number>()
+</script>
