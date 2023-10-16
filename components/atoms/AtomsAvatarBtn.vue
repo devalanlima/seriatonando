ba60d688a8d7687799c88424248052cfbb1757ca
Alan Lima <dev.alanlima@gmail.com>
2023-10-16 17:41:58 -0300

    feat: Create avatar button

---
+++ components/atoms/AtomsAvatarBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="w-12 h-12 rounded-full overflow-hidden" @click="action">
+    <img src="https://placehold.co/600x600" alt="Profile Picture">
+  </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['action'])
+
+const action = ()=>emit('action')
+
+</script>
+
+<style scoped>
+
+</style>
