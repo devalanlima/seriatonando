de0917c9b039bcd843dd94a1b260d8ab13f78e03
Alan Lima <dev.alanlima@gmail.com>
2023-10-17 12:16:07 -0300

    style: change icon and ipunt paddings

--- components/atoms/AtomsSearchbar.vue
+++ components/atoms/AtomsSearchbar.vue
@@ -3,7 +3,7 @@
     <slot name="Label">
       <p class="-top-[35px] font-semibold absolute" v-if="props.label">{{ props.label }}</p>
     </slot>
-    <div class="stroke-white fill-white z-10 px-2 w-fit cursor-pointer hover:scale-110">
+    <div class="stroke-white fill-white z-10 px-2 pl-3 w-fit cursor-pointer hover:scale-110">
       <slot name="icon">
         <AtomsIconsMagnifying
           width="w-5"
@@ -16,7 +16,7 @@
       type="search"
       :placeholder="props.placeholder"
       v-model="value"
-      class="absolute z-0 outline-none border-b-[1px] focus:border-b-2 box-decoration-slice bg-gradient-to-r from-color_primary/50 focus:from-color_primary to-transparent border-color_primary bg-transparent text-white p-4 pb-3 placeholder:text-white/80 pl-9 w-full"
+      class="absolute z-0 outline-none border-b-[1px] focus:border-b-2 box-decoration-slice bg-gradient-to-r from-color_primary/50 focus:from-color_primary to-transparent border-color_primary bg-transparent text-white p-4 pb-3 placeholder:text-white/80 pl-10 w-full"
     >
   </label>
 </template>
