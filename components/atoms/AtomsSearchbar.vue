f5f7676776caa120168bea4360bfcc277ed55d64
Alan Lima <dev.alanlima@gmail.com>
2023-10-17 12:10:48 -0300

    style: Fix icon props and change label height

--- components/atoms/AtomsSearchbar.vue
+++ components/atoms/AtomsSearchbar.vue
@@ -1,13 +1,14 @@
 <template>
-  <label class="text-color_primary flex flex-col relative justify-center max-w-[500px] cursor-text">
+  <label class="text-color_primary h-12 mb-[6px] flex flex-col relative justify-center cursor-text">
     <slot name="Label">
       <p class="-top-[35px] font-semibold absolute" v-if="props.label">{{ props.label }}</p>
     </slot>
     <div class="stroke-white fill-white z-10 px-2 w-fit cursor-pointer hover:scale-110">
       <slot name="icon">
         <AtomsIconsMagnifying
-          :size="20"
-          class="stroke-white"
+          width="w-5"
+          stroke-color="stroke-white"
+          stroke-width="stroke-1"
         />
       </slot>
     </div>
@@ -15,7 +16,7 @@
       type="search"
       :placeholder="props.placeholder"
       v-model="value"
-      class="absolute z-0 outline-none border-b-[1px] focus:border-b-2 box-decoration-slice bg-gradient-to-r from-color_primary/50 focus:from-color_primary to-transparent border-color_primary bg-transparent text-white p-4 pb-3 placeholder:text-white/80 pl-9 w-full "
+      class="absolute z-0 outline-none border-b-[1px] focus:border-b-2 box-decoration-slice bg-gradient-to-r from-color_primary/50 focus:from-color_primary to-transparent border-color_primary bg-transparent text-white p-4 pb-3 placeholder:text-white/80 pl-9 w-full"
     >
   </label>
 </template>
@@ -29,7 +30,6 @@ interface Props {
 
 const props = withDefaults(defineProps<Props>(),{
   value: '',
-  placeholder: 'Placeholder',
 })
 
 const value = UseDebouncedRef(props.value)
