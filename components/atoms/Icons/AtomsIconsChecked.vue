c1ef984c43ed3d5cbaeccfb3becf4c4c2619ea3c
Alan Lima <dev.alanlima@gmail.com>
2023-10-19 14:56:01 -0300

    feat: Add icon

---
+++ components/atoms/Icons/AtomsIconsChecked.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="[props.fillColor, props.strokeColor, props.strokeWidth, props.width, props.height, props.aspect]">
+    <svg
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g
+        id="SVGRepo_bgCarrier"
+        stroke-width="2"
+      ></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M7 12L9.89075 14.8907V14.8907C9.95114 14.951 10.049 14.9511 10.1094 14.8907V14.8907L17 8"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+    </g>
+  </svg>
+</div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  fillColor?: string;
+  strokeColor?: string;
+  strokeWidth?: string;
+  width?: string;
+  height?: string;
+  aspect?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  fillColor: 'fill-neutral-500',
+  strokeColor: 'stroke-red-300',
+  aspect: 'aspect-square',
+  strokeWidth: 'stroke-0'
+})
+</script>
