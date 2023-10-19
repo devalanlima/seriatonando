c39270b876bb6dd582e91ffce627cf34b2e0a6cf
Alan Lima <dev.alanlima@gmail.com>
2023-10-19 14:52:16 -0300

    feat: Create checkbox component

---
+++ components/atoms/AtomsCheckbox.vue
@@ -0,0 +1,43 @@
+<template>
+  <label class="w-fit h-fit cursor-pointer" :title="props?.title">
+    <span class="sr-only">{{ props?.title }}</span>
+    <input
+      type="checkbox"
+      class="peer sr-only"
+      v-model="isChecked"
+    >
+    <div :class="['rounded-md border-2 h-fit w-fit cursor-pointer', isChecked? 'bg-color_primary  border-color_primary' : 'bg-color_secondary border-color_secondary']">
+      <AtomsIconsChecked
+      :stroke-color="isChecked? 'stroke-color_light' : 'stroke-color_terciary'"
+      width="w-5"
+      />
+    </div>
+  </label>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  modelValue?: boolean;
+  title?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: false,
+})
+
+
+const emit = defineEmits<{
+  'update:modelValue': [value: boolean];
+}>()
+
+const isChecked = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(isChecked) {
+    emit('update:modelValue', isChecked);
+  }
+})
+
+</script>
