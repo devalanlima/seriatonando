160195bebb7516083614e60b153b75cbc3834a3d
Alan Lima <dev.alanlima@gmail.com>
2023-10-22 17:43:20 -0300

    feat: Create component for button icons

---
+++ components/atoms/AtomsButtonIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="emit('action')" :title="props.title">
+    <component
+      :is="props.icon"
+      :fill-color="props.fillColor"
+      :width="props.width"
+    />
+  </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['action'])
+interface Props {
+  icon: object;
+  fillColor?: string;
+  width?: string;
+  title: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  fillColor: 'fill-color_primary',
+  width: 'w-[55px]'
+})
+</script>
+
+<style scoped></style>
