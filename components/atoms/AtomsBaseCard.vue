1492925ddcd21ce2923a1eba3ac871f653cb92d9
Alan Lima <dev.alanlima@gmail.com>
2023-10-25 14:10:33 -0300

    refactor: Add color props to bg

--- components/atoms/AtomsBaseCard.vue
+++ components/atoms/AtomsBaseCard.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="p-4 bg-color_terciary rounded-xl overflow-hiddeng">
+  <div :class="['p-4 rounded-xl overflow-hiddeng', props.color]">
     <slot/>
   </div>
 </template>
+
+<script setup lang="ts">
+interface Props {
+  color?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'bg-color_terciary'
+})
+
+</script>
