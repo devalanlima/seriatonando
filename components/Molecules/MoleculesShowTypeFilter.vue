3f06a1fbf61b99d39dceedf94153257c1d7afbec
Alan Lima <dev.alanlima@gmail.com>
2023-10-25 14:19:33 -0300

    feat: Create TV Show type select buttons

---
+++ components/Molecules/MoleculesShowTypeFilter.vue
@@ -0,0 +1,27 @@
+<template>
+  <AtomsBaseCard color="bg-transparent" class="outline outline-2 outline-color_primary flex gap-5">
+    <AtomsShowTypeLabelFilter
+      v-for="showType in showTypes"
+      :key="showType"
+      :title="showType"
+      :is-checked="selectedShowType.includes(showType)"
+    >
+      <input
+        type="radio"
+        :value="showType"
+        v-model="selectedShowType"
+        name="showTypes"
+        class="sr-only"
+      >
+    </AtomsShowTypeLabelFilter>
+  </AtomsBaseCard>
+</template>
+
+<script setup lang="ts">
+const selectedShowType = ref<'All' | 'Movies' | 'Series'>('All')
+
+const showTypes = ['All', 'Movies', 'Series']
+
+</script>
+
+<style scoped></style>
