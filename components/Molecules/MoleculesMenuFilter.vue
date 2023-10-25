52741bc7a4d1011c3f80e827ac94252b42b7182e
Alan Lima <dev.alanlima@gmail.com>
2023-10-25 14:18:38 -0300

    feat: Create base filter component

---
+++ components/Molecules/MoleculesMenuFilter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="w-fit flex flex-col relative"
+    v-auto-animate
+  >
+    <AtomsFilterBtn
+      :title="props.btnTitle"
+      v-model="isFilterOpen"
+    />
+    <LazyAtomsBaseCard
+      color="bg-color_highlight"
+      v-if="isFilterOpen"
+      class="absolute w-[300px] flex flex-col top-14 left-0 z-20 overflow-hidden shadow-md shadow-black/50"
+      ref="target"
+    >
+      <slot/>
+    </LazyAtomsBaseCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isFilterOpen = ref<boolean>(false)
+
+const target = ref(null)
+
+interface Props {
+  btnTitle: string;
+}
+
+const props = defineProps<Props>()
+
+onClickOutside(target, () => {
+  setTimeout(() => {
+    isFilterOpen.value = false
+  }, 0);
+})
+
+</script>
+
+<style scoped></style>
