457207e93b3150e0cb72bc7184f3b303bed24dfd
Alan Lima <dev.alanlima@gmail.com>
2023-10-26 11:59:41 -0300

    feat: Add components to app

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Hello World!</h1>
+  <header class="p-5">
+    <OrganismsBaseNavbar />
+  </header>
+  <main class="md:px-5 flex flex-col gap-5">
+    <div class="flex gap-5 w-full">
+      <MoleculesShowTypeFilter/>
+      <MoleculesProvidersSelection/>
+    </div>
+    <OrganismsFilters class="px-5 md:px-0"/>
+    <NuxtPage />
+  </main>
 </template>
+
+<script setup>
+</script>
