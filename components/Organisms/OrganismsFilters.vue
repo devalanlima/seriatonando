2fd385a7dfbfde8ecef471c0e72373a75f101ffc
Alan Lima <dev.alanlima@gmail.com>
2023-10-26 12:01:36 -0300

    feat: Create filters section

---
+++ components/Organisms/OrganismsFilters.vue
@@ -0,0 +1,13 @@
+<template>
+  <section id="OrganismsFilters" class="flex items-center justify-start gap-5 w-full relative">
+    <MoleculesAverageRatingFilter/>
+    <MoleculesReleaseYearFilter/>
+    <MoleculesGenresFilter/>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
