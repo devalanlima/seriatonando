6fdd2e0e39caa3139460a2d70b821b838306bdc8
Alan Lima <dev.alanlima@gmail.com>
2023-10-31 13:43:53 -0300

    feat: Add searchbar and adjust selected language btn style

--- components/Molecules/MoleculesLanguageSelectionMenu.vue
+++ components/Molecules/MoleculesLanguageSelectionMenu.vue
@@ -3,10 +3,14 @@
     class="h-fit flex flex-col py-0 px-0"
     color="bg-color_highlight"
   >
-    <AtomsMiniSearchbar
-      v-model="searchbarValue"
-      class="border-b-2 border-color_primary py-2"
-    />
+    <div v-if="atualLocale === locale ? true : false">
+      <AtomsDivisionLine />
+      <AtomsMiniSearchbar
+        v-model="searchbarValue"
+        class="rounded-e-none rounded-s-none"
+      />
+      <AtomsDivisionLine />
+    </div>
     <ul
       class="flex flex-wrap justify-between gap-2 overflow-y-scroll max-h-[50vh] min-w-[180px] relative bg-inherit overflow-x-hidden w-full"
     >
@@ -15,7 +19,7 @@
         :key="locale"
       >
         <li
-          :class="atualLocale === locale ? '-order-1 sticky pr-2 top-0 bg-inherit flex gap-2 items-center justify-between border-b-[2px] border-color_primary py-3 w-full' : 'pr-2 w-[100px]'"
+          :class="atualLocale === locale ? '-order-1 sticky pr-2 top-0 bg-inherit flex gap-2 items-center justify-between w-full border-b-[1px] border-color_primary py-2 pointer-events-none cursor-default' : 'pr-2 w-[100px]'"
           v-if="searchbarValue.length === 0 || atualLocale === locale ? true : locale.toLowerCase().includes(searchbarValue.toLowerCase()) ? true : false"
         >
           <AtomsLanguageBtn
