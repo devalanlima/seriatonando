1ae11a937de586beec2e890500d65352bd6f0a7c
Alan Lima <dev.alanlima@gmail.com>
2023-10-31 13:45:08 -0300

    feat: Create divison line component

---
+++ components/atoms/AtomsDivisionLine.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="['block', props.color, orientation]"></div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  color?: string;
+  orientation?: 'vertical' | 'horizontal'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: "bg-color_primary",
+  orientation: "horizontal",
+})
+
+const orientation = computed((): string => {
+  switch (true) {
+    case props.orientation === "horizontal":
+      return "w-full h-[1px]"
+      break;
+    case props.orientation === "vertical":
+      return "h-full w-[1px]"
+      break;
+    default:
+    return ""
+      break;
+  }
+})
+</script>
