77a64a261b831871c19eea70e45c5ca1072808ba
Alan Lima <dev.alanlima@gmail.com>
2023-10-31 11:48:50 -0300

    feat: Create mini searcbar component

---
+++ components/atoms/AtomsMiniSearchbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <label
+    class="flex h-full items-center w-full"
+  >
+    <AtomsIconsMagnifying
+      class="cursor-pointer absolute"
+      width="w-[13px]"
+      stroke-color="stroke-color_primary"
+      stroke-width="stroke-1"
+    />
+    <input
+      type="text"
+      v-model="searchbarValue"
+      class="bg-transparent focus:outline-none pl-5 w-full"
+    >
+  </label>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: string;
+}
+
+const Props = defineProps<Props>()
+
+const emit = defineEmits<{
+  'update:modelValue': [value: string]
+}>()
+
+const searchbarValue = computed({
+  get(){
+    return Props.modelValue
+  },
+  set(searchbarValue){
+    emit('update:modelValue', searchbarValue)
+  }
+})
+</script>
