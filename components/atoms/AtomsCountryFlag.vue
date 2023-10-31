b421e86c50e51ecf0d1cda0fca36ef1bccec9e1f
Alan Lima <dev.alanlima@gmail.com>
2023-10-31 11:46:23 -0300

    feat: Create country flags component

---
+++ components/atoms/AtomsCountryFlag.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img class="aspect-square" :src="path" :alt="`${regionCode} Flag`">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  countryCode: string;
+}
+
+const props = defineProps<Props>();
+
+const regionCode = computed(() => {
+  if (props.countryCode.length > 2) {
+    return props.countryCode.split('-')[1].toUpperCase();
+  } else {
+    return props.countryCode.toUpperCase();
+  }
+});
+
+const path = computed(()=> `/countryFlags/${regionCode.value}.svg`)
+
+</script>
+
+<!-- flags SVG : https://github.com/hampusborgos/country-flags/tree/main -->
