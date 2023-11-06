963c6e9a66bc92c2dc925856bb8fbb5211dfe366
Alan Lima <dev.alanlima@gmail.com>
2023-11-06 10:54:13 -0300

    refactor: change types and logic to work with boolean values

--- components/Atoms/AtomsAgeGroupBtn.vue
+++ components/Atoms/AtomsAgeGroupBtn.vue
@@ -1,7 +1,7 @@
 <template>
   <label
     :class="['block p-1 outline outline-1 text-center cursor-pointer rounded-lg', isChecked ? 'outline-color_primary outline-2 text-color_primary font-semibold' : 'outline-color_secondary text-color_secondary']"
-    :title="props.inputValue"
+    :title="props.title"
   >
     <p class="whitespace-nowrap">{{ props.inputName }}</p>
     <input
@@ -16,12 +16,17 @@
 <script setup lang="ts">
 interface Props {
   inputName: string;
-  inputValue: string;
-  modelValue: Array<string>;
+  inputValue: string | boolean;
+  modelValue: Array<string> | boolean;
+  title: string;
 }
 
 const isChecked = computed(() => {
-  return selectedValues.value.includes(props.inputValue)
+  if (typeof selectedValues.value != 'boolean' && typeof props.inputValue != 'boolean') {
+    return selectedValues.value.includes(props.inputValue)
+  } else {
+    return selectedValues.value
+  }
 })
 
 const props = defineProps<Props>()
