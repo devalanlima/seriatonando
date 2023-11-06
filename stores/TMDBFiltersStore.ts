453642bef5902400dfdaa7b92b70d5338985a417
Alan Lima <dev.alanlima@gmail.com>
2023-11-06 15:00:33 -0300

    feat: Add show type in store

--- stores/TMDBFiltersStore.ts
+++ stores/TMDBFiltersStore.ts
@@ -5,7 +5,13 @@ export const useTMDBFiltersStore = defineStore("TMDBFiltersStore", () => {
   const projectStore = useProjectStore()
   const watchRegion =  computed<Regions>(()=> projectStore.region)
 
-  return { watchRegion }
+  const showType =  ref<ShowType>("all")
+  const setShowType = (selectedShowtype: ShowType) => {
+    showType.value = selectedShowtype
+  }
+
+
+  return { watchRegion, showType, setShowType }
 })
 if(import.meta.hot){
   import.meta.hot.accept(acceptHMRUpdate(useTMDBFiltersStore, import.meta.hot));
