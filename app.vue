6db50340ef77b8c7499aae48b41431c992cb609b
Alan Lima <dev.alanlima@gmail.com>
2023-11-07 16:28:27 -0300

    feat: Add nuxt layout

--- app.vue
+++ app.vue
@@ -1,16 +1,7 @@
 <template>
-  <NuxtLoadingIndicator :throttle="0" color="#E71270" />
-  <header class="p-5">
-    <OrganismsBaseNavbar />
-  </header>
-  <main class="md:px-5 flex flex-col gap-5">
-    <div class="flex px-5 md:px-0 gap-5 w-full items-center">
-      <MoleculesShowTypeFilter/>
-      <OrganismsProvidersSelection/>
-    </div>
-    <OrganismsFilters class="px-5 md:px-0"/>
+  <NuxtLayout>
     <NuxtPage />
-  </main>
+  </NuxtLayout>
 </template>
 
 <script setup>
