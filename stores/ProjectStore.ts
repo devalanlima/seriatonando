91f789c257140c420dd24a79ffbe3790cb38074b
Alan Lima <dev.alanlima@gmail.com>
2023-11-07 16:18:58 -0300

    feat: Add language

--- stores/ProjectStore.ts
+++ stores/ProjectStore.ts
@@ -5,7 +5,9 @@ export const useProjectStore = defineStore("ProjectStore", () => {
   const region = computed<Regions>(() => i18n.locale.value.slice(i18n.locale.value.indexOf('-') + 1) as Regions)
   const defaultRegion = computed<Regions>(() => i18n.defaultLocale.slice(i18n.defaultLocale.indexOf('-') + 1) as Regions)
 
-  return { region, defaultRegion }
+  const language = computed(()=>i18n.locale.value)
+
+  return { region, defaultRegion, language }
 })
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useProjectStore, import.meta.hot));
