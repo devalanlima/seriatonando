77fcffe018a1af4ab596fdf8bf074141b3275f13
Alan Lima <dev.alanlima@gmail.com>
2023-11-07 16:26:58 -0300

    feat: Add api call and populated component card

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,93 @@
 <template>
-  <div class="grid grid-cols-[repeat(auto-fit,_minmax(375px,_1fr))] place-items-center gap-5">
-    <OrganismsMovieCard v-for="item in 20" />
-  </div>
+  <template v-if="pending">
+    <OrganismsMovieCard
+      v-for="load in 20"
+      :key="load"
+      movie-title="Loading..."
+      :movie-genres="[0]"
+      movie-overview="Loading..."
+      poster-path="loading..."
+    />
+  </template>
+  <template v-else>
+    <OrganismsMovieCard
+      v-for="movie in movies?.results"
+      :key="movie.id"
+      :movie-title="(typeof movie.title === 'string' ? movie.title : typeof movie.name === 'string' ? movie.name : movie.original_title)"
+      :movie-genres="movie.genre_ids"
+      :movie-overview="movie.overview"
+      :poster-path="movie.poster_path"
+    />
+  </template>
 </template>
 
 <script setup lang="ts">
+interface Show {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: Array<number>;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title?: string;
+  name?: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Discover {
+  page: number;
+  results: Array<Show>;
+  total_pageS: number;
+  total_results: number
+}
+
+const projectStore = useProjectStore()
+const tmdbFiltersStore = useTMDBFiltersStore()
+
+import { computed, ref, watch } from 'vue';
+
+const params = computed(() => {
+  return {
+    "language": projectStore.language,
+    "certification_country": projectStore.region,
+    "page": 1,
+    "region": projectStore.region,
+    "release_date.gte": tmdbFiltersStore.releaseDateGte,
+    "release_date.lte": tmdbFiltersStore.releaseDateLte,
+    "sort_by": "popularity.desc",
+    "vote_average.gte": tmdbFiltersStore.voteAverageGte,
+    "vote_average.lte": tmdbFiltersStore.voteAverageLte,
+    "watch_region": projectStore.region,
+  };
+});
+
+const debounceParams = ref(params.value);
+let timeoutId: ReturnType<typeof setTimeout>;
+const debounce = <T>(item: T, delay: number): Promise<T> => {
+  return new Promise<T>((resolve) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      resolve(item);
+    }, delay);
+  });
+};
+
+watch(() => params.value, () => {
+  debounce(params.value, 500).then((result) => {
+    debounceParams.value = result;
+  });
+});
+
+
+const { data: movies, pending } = await useFetch<Discover>('/api/TMDB', {
+  params: debounceParams,
+})
+
 
 </script>
-
-<style scoped></style>
