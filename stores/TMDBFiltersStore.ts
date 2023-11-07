f7dd7ed31471ab26f6aba1e5074cce741fe74684
Alan Lima <dev.alanlima@gmail.com>
2023-11-07 10:34:07 -0300

    feat: Add genres array

--- stores/TMDBFiltersStore.ts
+++ stores/TMDBFiltersStore.ts
@@ -1,36 +1,38 @@
 import { useProjectStore } from "./ProjectStore";
 
 export const useTMDBFiltersStore = defineStore("TMDBFiltersStore", () => {
+  //watch region
+  const projectStore = useProjectStore();
+  const watchRegion = computed<Regions>(() => projectStore.region);
 
-  const projectStore = useProjectStore()
-  const watchRegion = computed<Regions>(() => projectStore.region)
-
-
-  const showType = ref<ShowType>("all")
+  //show type movie/tv
+  const showType = ref<ShowType>("all");
   const setShowType = (selectedShowtype: ShowType) => {
-    showType.value = selectedShowtype
+    showType.value = selectedShowtype;
   }
 
-
-  const releaseYearLte = ref<number>(Number(new Date().getFullYear()))
-  const releaseYearGte = ref<number>(1865)
+  //release year
+  const releaseYearLte = ref<number>(Number(new Date().getFullYear()));
+  const releaseYearGte = ref<number>(1865);
   const releaseDateLte = computed<string>(()=>{
     if (releaseYearLte.value === new Date().getFullYear()) {
-      return `${releaseYearLte.value}-${new Date().getMonth() + 1}-${new Date().getDate()}`
+      return `${releaseYearLte.value}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
     } else {
-      return `${releaseYearLte.value}-12-31`
+      return `${releaseYearLte.value}-12-31`;
     }
   })
-  const releaseDateGte = computed<string>(()=> `${releaseYearGte.value}-01-01`)
+  const releaseDateGte = computed<string>(()=> `${releaseYearGte.value}-01-01`);
 
-
+  //vote average
   const voteAverageLte = ref<number>(10);
   const voteAverageGte = ref<number>(0);
 
+  //genres
+  const genres = ref<Array<number>>([]);
 
 
-
-  return { watchRegion, showType, setShowType, releaseDateLte, releaseDateGte, releaseYearGte, releaseYearLte, voteAverageGte, voteAverageLte }
+  
+  return { watchRegion, showType, setShowType, releaseDateLte, releaseDateGte, releaseYearGte, releaseYearLte, voteAverageGte, voteAverageLte, genres };
 })
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useTMDBFiltersStore, import.meta.hot));
