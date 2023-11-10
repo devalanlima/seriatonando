d067292ffd12f19c8b864cb707d8e7a509bb06b4
Alan Lima <dev.alanlima@gmail.com>
2023-11-10 16:08:50 -0300

    refactor: Toggle logic to work with new api routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <template v-if="pending">
+  <template v-if="moviePending && tvPending">
     <OrganismsMovieCard
       v-for="load in 20"
       :key="load"
@@ -11,83 +11,75 @@
   </template>
   <template v-else>
     <OrganismsMovieCard
-      v-for="movie in movies?.results"
+      v-for="movie in allShows.sort((a, b) => b.popularity - a.popularity)"
       :key="movie.id"
-      :movie-title="(typeof movie.title === 'string' ? movie.title : typeof movie.name === 'string' ? movie.name : movie.original_title)"
-      :movie-genres="movie.genre_ids"
+      :movie-title="movie.title"
+      :movie-genres="movie.genres"
       :movie-overview="movie.overview"
-      :poster-path="(movie.poster_path != null? movie.poster_path: 'not found')"
+      :poster-path="(movie.posterPath != null ? movie.posterPath : 'not found')"
     />
   </template>
 </template>
 
 <script setup lang="ts">
-interface Show {
-  adult: boolean;
-  backdrop_path: string;
-  genre_ids: Array<number>;
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
-  title?: string;
-  name?: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
-
-interface Discover {
-  page: number;
-  results: Array<Show>;
-  total_pageS: number;
-  total_results: number
-}
+import useDebouncedComputed from '~/composables/UseDebouncedComputed';
 
 const projectStore = useProjectStore()
 const tmdbFiltersStore = useTMDBFiltersStore()
 
-import { computed, ref, watch } from 'vue';
 
-const params = computed(() => {
+const movieParams = computed(() => {
   return {
     "language": projectStore.language,
     "certification_country": projectStore.region,
     "page": 1,
     "region": projectStore.region,
-    "release_date.gte": tmdbFiltersStore.releaseDateGte,
-    "release_date.lte": tmdbFiltersStore.releaseDateLte,
-    "sort_by": "popularity.desc",
+    "primary_release_date.gte": tmdbFiltersStore.releaseDateGte,
+    "primary_release_date.lte": tmdbFiltersStore.releaseDateLte,
+    "sort_by": tmdbFiltersStore.sortBy,
     "vote_average.gte": tmdbFiltersStore.voteAverageGte,
     "vote_average.lte": tmdbFiltersStore.voteAverageLte,
     "watch_region": projectStore.region,
+    "vote_count.gte": 100,
+    "with_watch_providers": tmdbFiltersStore.providers.join('|'),
+    "certification": tmdbFiltersStore.certifications.join('|'),
+    "with_genres": tmdbFiltersStore.genres.join('|'),
   };
 });
 
-const debounceParams = ref(params.value);
-let timeoutId: ReturnType<typeof setTimeout>;
-const debounce = <T>(item: T, delay: number): Promise<T> => {
-  return new Promise<T>((resolve) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      resolve(item);
-    }, delay);
-  });
-};
-
-watch(() => params.value, () => {
-  debounce(params.value, 500).then((result) => {
-    debounceParams.value = result;
-  });
+const tvParams = computed(() => {
+  return {
+    "language": projectStore.language,
+    "page": 1,
+    "region": projectStore.region,
+    "air_date.gte": tmdbFiltersStore.releaseDateGte,
+    "air_date.lte": tmdbFiltersStore.releaseDateLte,
+    "sort_by": tmdbFiltersStore.sortBy,
+    "vote_average.gte": tmdbFiltersStore.voteAverageGte,
+    "vote_average.lte": tmdbFiltersStore.voteAverageLte,
+    "watch_region": projectStore.region,
+    "vote_count.gte": 100,
+    "with_watch_providers": tmdbFiltersStore.providers.join('|'),
+    "with_genres": tmdbFiltersStore.genres.join('|'),
+  };
 });
 
+const debouncedMovieParams = useDebouncedComputed(movieParams);
+const debouncedTvParams = useDebouncedComputed(tvParams);
 
-const { data: movies, pending } = await useFetch<Discover>('/api/TMDB', {
-  params: debounceParams,
+const { data: movieShow, pending: moviePending } = await UseCallCustomApiResponse('/api/TMDBDiscoverMovie', debouncedMovieParams);
+const { data: tvShow, pending: tvPending } = await UseCallCustomApiResponse('/api/TMDBDiscoverTv', debouncedTvParams);
+
+const allShows = computed<Array<MoviesAndTv>>(() => {
+  let all: Array<MoviesAndTv> = []
+  if (tmdbFiltersStore.showType === 'all') {
+    all = [...movieShow.value as Array<MoviesAndTv>, ...tvShow.value as Array<MoviesAndTv>]
+  } else if (tmdbFiltersStore.showType === 'movies') {
+    all = [...movieShow.value as Array<MoviesAndTv>]
+  } else if (tmdbFiltersStore.showType === 'tv') {
+    all = [...tvShow.value as Array<MoviesAndTv>]
+  }
+  return all;
 })
-
 
 </script>
