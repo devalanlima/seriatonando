0d5481de1fd948b41626a6ef2b9052d4eac78589
Alan Lima <dev.alanlima@gmail.com>
2023-11-10 16:02:25 -0300

    feat: Add MovieShow, TvShow and Discover interfaces

--- types/TMDBFilterResponses.d.ts
+++ types/TMDBFilterResponses.d.ts
@@ -57,6 +57,48 @@ declare global {
   interface Genres {
     "genres": Array<Genre>;
   }
+
+
+
+  interface MovieShow {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: Array<number>;
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  }
+
+  interface TvShow {
+    backdrop_path: string;
+    first_air_date: string;
+    genre_ids: Array<number>;
+    id: number;
+    name: string;
+    origin_country: Array<string>;
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    vote_average: number;
+    vote_count: number;
+  }
+
+  interface Discover {
+    page: number;
+    results: Array<MovieShow> | Array<TvShow>;
+    total_pageS: number;
+    total_results: number
+  }
 }
 export { };
 
