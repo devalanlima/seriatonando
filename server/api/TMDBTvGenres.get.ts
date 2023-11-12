310f5df20e32f89f19ab25acbc6489a0e97ac3d0
Alan Lima <dev.alanlima@gmail.com>
2023-11-12 11:31:01 -0300

    feat: Create endpoint to call genres from tmdb server

---
+++ server/api/TMDBTvGenres.get.ts
@@ -0,0 +1,18 @@
+const { public: BASE, TMDB_KEY } = useRuntimeConfig()
+
+export default defineEventHandler(async (event) => {
+
+const params = getQuery(event)
+
+
+  const data:Genres = await $fetch(`${BASE.TMDB_URL}/genre/tv/list`, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: TMDB_KEY
+    },
+    params: params
+  })
+
+  return data
+})
