9ebe9859a489d508768508a51162022851df7411
Alan Lima <dev.alanlima@gmail.com>
2023-11-22 11:59:34 -0300

    feat: Add total pages and total result in response

--- composables/UseCallCustomApiResponse.ts
+++ composables/UseCallCustomApiResponse.ts
@@ -3,16 +3,20 @@ export default async function useCallCustomApiResponse(url: string, params: obje
     params: params,
     key: url + Math.random().toString ,
     transform: (item: Discover) => {
-      return item.results.map((item): MoviesAndTv => ({
+      const results = item.results.map((item): MoviesAndTv => ({
         id: item.id,
         title: 'title' in item? item.title : item.name,
         genres: item.genre_ids,
         overview: item.overview,
         posterPath: item.poster_path,
         popularity: item.popularity,
-        showType: 'title' in item? 'movies' : 'tv',
+        showType: 'title' in item? 'movies' : 'name' in item? 'tv': 'all',
       }));
+      const totalPages  = item.total_pages;
+      const totalResults = item.total_results;
+      return {results, totalResults, totalPages};
     },
+    watch: false
   })
   return {data, pending, error}
 }
