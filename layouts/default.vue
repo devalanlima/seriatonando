56ba897f4b6c334fe27478be7bf60ad925ec1409
Alan Lima <dev.alanlima@gmail.com>
2023-11-26 18:57:35 -0300

    feat: Conditionally hides filters and providers selection

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,20 @@
 <template>
-    <NuxtLoadingIndicator
-      :throttle="0"
-      color="#E71270"
+  <NuxtLoadingIndicator
+    :throttle="0"
+    color="#E71270"
+  />
+  <header class="p-5">
+    <OrganismsBaseNavbar />
+  </header>
+  <main class="md:px-5 flex flex-col gap-5">
+    <div class="flex px-5 md:px-0 gap-5 w-full items-center">
+      <MoleculesShowTypeFilter />
+      <OrganismsProvidersSelection
+        v-if="$route.fullPath.split('/')[$route.fullPath.split('/').length - 1] !== 'trending'" />
+    </div>
+    <OrganismsFilters
+      class="px-5 md:px-0"
+      v-if="$route.fullPath.split('/')[$route.fullPath.split('/').length -1] !== 'trending'"
     />
-    <header class="p-5">
-      <OrganismsBaseNavbar />
-    </header>
-    <main class="md:px-5 flex flex-col gap-5">
-      <div class="flex px-5 md:px-0 gap-5 w-full items-center">
-        <MoleculesShowTypeFilter />
-        <OrganismsProvidersSelection />
-      </div>
-      <OrganismsFilters class="px-5 md:px-0" />
-      <slot></slot>
-    </main>
-</template>
+  <slot></slot>
+</main></template>
