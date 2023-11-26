f5629781281093f892fff770c94e71b302278687
Alan Lima <dev.alanlima@gmail.com>
2023-11-26 19:14:48 -0300

    feat: Add trending

--- pages/trending/index.vue
+++ pages/trending/index.vue
@@ -1,10 +1,27 @@
 <template>
+  <OrganismsShowsGridLayout
+    :movie-params="movieParams"
+    :tv-params="tvParams"
+    url-api-movie="/api/TMDBTrendingMovie"
+    url-api-tv="/api/TMDBTrendingTv"
+  />
 </template>
 
 <script setup lang="ts">
+const projectStore = useProjectStore();
+const tmdbFiltersStore = useTMDBFiltersStore();
 
+const movieParams = computed(() => {
+  return {
+    "language": projectStore.language,
+    "page": tmdbFiltersStore.moviePage,
+  };
+});
+
+const tvParams = computed(() => {
+  return {
+    "language": projectStore.language,
+    "page": tmdbFiltersStore.tvPage,
+  };
+});
 </script>
-
-<style scoped>
-
-</style>
