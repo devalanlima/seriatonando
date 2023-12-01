283d1f82bb2142ba800f64f1b3765d1eaeafa825
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 14:15:55 -0300

    feat: Create component to show provider logo

---
+++ components/Atoms/AtomsProviderData.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img
+      class="max-w-[50px] rounded-xl"
+      :title="props.title"
+      :src="`https://image.tmdb.org/t/p/w500/${props.path}`"
+      :alt="props.title"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  path: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
