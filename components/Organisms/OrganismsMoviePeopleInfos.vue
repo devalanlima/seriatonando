efc70dfd13b791926b34a60d5857120b1df09fac
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 14:38:03 -0300

    feat: Create component to movie infos

---
+++ components/Organisms/OrganismsMoviePeopleInfos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-2 border-color_primary">
+    <h2 class="font-semibold text-xl text-color_primary">{{ props.title }}</h2>
+    <MoleculesPeopleCarousel :people="props.people" />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  people: Array<People>;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped></style>
