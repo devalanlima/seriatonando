1f02d1562a43e3388940acae52508f8c9afc0098
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 13:59:41 -0300

    feat: Create component for youtube iframe

---
+++ components/Atoms/AtomsYouTubeIframe.vue
@@ -0,0 +1,23 @@
+<template>
+    <iframe
+      class="border-color_primary aspect-video"
+      width="100%"
+      height="100%"
+      :src="`https://www.youtube.com/embed/${props.videoKey}?${props.videoId}`"
+      :title="props.title"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    >
+    </iframe>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  videoKey: string;
+  videoId: string;
+}
+
+const props = defineProps<Props>();
+</script>
