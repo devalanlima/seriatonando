a6852429dceccadbe1bcb5e64b53e8b3f96aeecf
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 13:58:55 -0300

    feat: Create card containing name and role to people

---
+++ components/Atoms/AtomsPeopleCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <img
+      class="min-w-[150px] max-h-[225px]"
+      :src="props.path !== null ? `https://image.tmdb.org/t/p/original${props.path}` : 'https://via.placeholder.com/150x225'"
+      alt=""
+    >
+    <div>
+      <p class="text-color_primary font-semibold">{{ props.name }}</p>
+      <p class="text-sm text-color_secondary">{{ props.role }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string;
+  role: string;
+  path: string | null;
+}
+const props = defineProps<Props>();
+</script>
