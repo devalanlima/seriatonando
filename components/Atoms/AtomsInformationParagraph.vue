ec2b0d04c1e1ebf5307fe4f7c5a202380c0c296a
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 12:30:35 -0300

    feat: Create paragraph component to infos

---
+++ components/Atoms/AtomsInformationParagraph.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="py-5 border-color_primary">
+    <h2 class="font-semibold text-xl text-color_primary">{{ props.title }}</h2>
+    <p class="dark:text-color_light text-color_terciary dark:font-normal font-semibold">{{ props.paragraph }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+  paragraph: string;
+}
+
+const props = defineProps<Props>();
+</script>
