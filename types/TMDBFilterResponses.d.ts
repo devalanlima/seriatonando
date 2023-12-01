6f933d17efc5e7d9df339fad4414acbd1311374a
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 12:29:36 -0300

    feat: Add MovieId interface and ads in WatchProvidersResults

--- types/TMDBFilterResponses.d.ts
+++ types/TMDBFilterResponses.d.ts
@@ -38,6 +38,7 @@ declare global {
   interface WatchProvidersResults {
     [key: string]: {
       "link": string;
+      "ads"?: Array<WatchProvidersRegionContent>;
       "flatrate"?: Array<WatchProvidersRegionContent>;
       "buy"?: Array<WatchProvidersRegionContent>;
       "rent"?: Array<WatchProvidersRegionContent>;
@@ -99,6 +100,98 @@ declare global {
     total_pages: number;
     total_results: number
   }
+
+
+  interface MovieId {
+    adult: boolean;
+    backdrop_path: string;
+    belongs_to_collection: null | any;
+    budget: number;
+    genres: Array<{ id: number; name: string }>;
+    homepage: string;
+    id: number;
+    imdb_id: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    production_companies: Array<{
+      id: number;
+      logo_path: string | null;
+      name: string;
+      origin_country: string;
+    }>;
+    production_countries: Array<{ iso_3166_1: string; name: string }>;
+    release_date: string;
+    revenue: number;
+    runtime: number;
+    spoken_languages: Array<{ english_name: string; iso_639_1: string; name: string }>;
+    status: string;
+    tagline: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    "watch/providers": {results: WatchProvidersResults};
+    release_dates: {
+      results: Array<{
+        iso_3166_1: string;
+        release_dates: Array<{
+          certification: string;
+          descriptors: string[];
+          iso_639_1: string;
+          note: string;
+          release_date: string;
+          type: number;
+        }>;
+      }>;
+    },
+    credits: {
+      cast: Array<{
+        adult: boolean;
+        gender: number;
+        id: number;
+        known_for_department: string;
+        name: string;
+        original_name: string;
+        popularity: number;
+        profile_path: string;
+        cast_id: number;
+        character: string;
+        credit_id: string;
+        order: number;
+      }>;
+      crew: Array<{
+        adult: boolean;
+        gender: number;
+        id: number;
+        known_for_department: string;
+        name: string;
+        original_name: string;
+        popularity: number;
+        profile_path: string;
+        credit_id: string;
+        department: string;
+        job: string;
+      }>;
+    },
+    videos: {
+      results: Array<{
+        iso_639_1: string;
+        iso_3166_1: string;
+        name: string;
+        key: string;
+        site: string;
+        size: number;
+        type: string;
+        official: boolean;
+        published_at: string;
+        id: string;
+      }>
+    }
+  }
 }
+
 export { };
 
