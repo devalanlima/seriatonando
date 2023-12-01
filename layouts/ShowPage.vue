c2c74fa76f26426108b5c1a47160ab25b8cc7fd5
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 14:45:59 -0300

    feat: Create layout for show page

---
+++ layouts/ShowPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLoadingIndicator
+    :throttle="0"
+    color="#E71270"
+  />
+  <header class="p-5">
+    <OrganismsBaseNavbar />
+  </header>
+  <main class="md:px-5 flex flex-col gap-5">
+    
+    <slot></slot>
+  </main>
+</template>
