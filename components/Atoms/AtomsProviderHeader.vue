3c23de6c765f00d9a0cf481064a46602cc642f2b
Alan Lima <dev.alanlima@gmail.com>
2023-12-01 14:17:31 -0300

    feat: Create title for row in providers table

---
+++ components/Atoms/AtomsProviderHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-[100px] w-[30px] flex justify-center sticky left-0">
+    <span class="-rotate-90 grid place-items-center">
+      {{ props.title }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string;
+}
+
+const props = defineProps<Props>();
+</script>
