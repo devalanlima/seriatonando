c5aeb253d698beda834a1f846b86f624e2a98859
Alan Lima <dev.alanlima@gmail.com>
2023-12-04 19:05:57 -0300

    feat: Add TVId interface

--- types/TMDBFilterResponses.d.ts
+++ types/TMDBFilterResponses.d.ts
@@ -133,20 +133,20 @@ declare global {
     video: boolean;
     vote_average: number;
     vote_count: number;
-    "watch/providers": {results: WatchProvidersResults};
+    "watch/providers": { results: WatchProvidersResults };
     release_dates: {
       results: Array<{
         iso_3166_1: string;
         release_dates: Array<{
           certification: string;
-          descriptors: string[];
+          descriptors: Array<string>;
           iso_639_1: string;
           note: string;
           release_date: string;
           type: number;
         }>;
       }>;
-    },
+    };
     credits: {
       cast: Array<{
         adult: boolean;
@@ -175,7 +175,7 @@ declare global {
         department: string;
         job: string;
       }>;
-    },
+    };
     videos: {
       results: Array<{
         iso_639_1: string;
@@ -190,6 +190,155 @@ declare global {
         id: string;
       }>
     }
+  }
+
+  interface TVId {
+    adult: boolean;
+    "watch/providers": { results: WatchProvidersResults };
+    videos: {
+      results: Array<{
+        iso_639_1: string;
+        iso_3166_1: string;
+        name: string;
+        key: string;
+        site: string;
+        size: number;
+        type: string;
+        official: boolean;
+        published_at: string;
+        id: string;
+      }>
+    };
+    content_ratings: {
+      results: Array<{
+        descriptos: Array<string>;
+        iso_3166_1: string;
+        rating: string;
+      }>;
+    };
+    credits: {
+      cast: Array<{
+        adult: boolean;
+        gender: number;
+        id: number;
+        known_for_department: string;
+        name: string;
+        original_name: string;
+        popularity: number;
+        profile_path: string;
+        cast_id: number;
+        character: string;
+        credit_id: string;
+        order: number;
+      }>;
+      crew: Array<{
+        adult: boolean;
+        gender: number;
+        id: number;
+        known_for_department: string;
+        name: string;
+        original_name: string;
+        popularity: number;
+        profile_path: string;
+        credit_id: string;
+        department: string;
+        job: string;
+      }>;
+    };
+    backdrop_path: string;
+    created_by: Array<{
+      id: number;
+      credit_id: string;
+      name: string;
+      gender: number;
+      profile_path: string;
+    }>;
+    episode_run_time: Array<number>;
+    first_air_date: string;
+    genres: Array<{
+      id: number;
+      name: string;
+    }>;
+    homepage: string;
+    id: number;
+    in_production: boolean;
+    languages: Array<string>;
+    last_air_date: string;
+    last_episode_to_air: {
+      id: number;
+      name: string;
+      overview: string;
+      vote_average: number;
+      vote_count: number;
+      air_date: string;
+      episode_number: number;
+      episode_type: string;
+      production_code: string;
+      runtime: number;
+      season_number: number;
+      show_id: number;
+      still_path: string;
+    };
+    name: string;
+    next_episode_to_air: {
+      id: number;
+      name: string;
+      overview: string;
+      vote_average: number;
+      vote_count: number;
+      air_date: string;
+      episode_number: number;
+      episode_type: string;
+      production_code: string;
+      runtime: number;
+      season_number: number;
+      show_id: number;
+      still_path: string;
+    };
+    networks: Array<{
+      id: number;
+      logo_path: string;
+      name: string;
+      origin_country: string;
+    }>;
+    number_of_episodes: number;
+    number_of_seasons: number;
+    origin_country: Array<string>;
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    production_companies: Array<{
+      id: number;
+      logo_path: string;
+      name: string;
+      origin_country: string;
+    }>;
+    production_countries: Array<{
+      iso_3166_1: string;
+      name: string;
+    }>;
+    seasons: Array<{
+      air_date: string;
+      episode_count: number;
+      id: number;
+      name: string;
+      overview: string;
+      poster_path: string;
+      season_number: number;
+      vote_average: number;
+    }>;
+    spoken_languages: Array<{
+      english_name: string;
+      iso_639_1: string;
+      name: string;
+    }>;
+    status: string;
+    tagline: string;
+    type: string;
+    vote_average: number;
+    vote_count: number;
   }
 }
 
