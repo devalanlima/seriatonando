25833ad43f52cfa63b3783a1bdecc5d1e7ac0e38
Alan Lima <dev.alanlima@gmail.com>
2023-12-12 10:59:01 -0300

    refactor: Change from checkbox to button

--- components/Atoms/AtomsCloseButton.vue
+++ components/Atoms/AtomsCloseButton.vue
@@ -1,36 +1,14 @@
 <template>
-  <label class="w-fit rounded-md bg-color_primary cursor-pointer">
+  <button class="w-fit rounded-md bg-color_primary cursor-pointer" @click="emit('action')">
     <AtomsIconsCross
       width="w-5"
       stroke-width="stroke-2"
       fill-color="fill-color_terciary"
       stroke-color="stroke-color_terciary"
     />
-    <input type="checkbox" class="sr-only" v-model="isChecked">
-  </label>
+  </button>
 </template>
 
 <script setup lang="ts">
-
-const isChecked = computed({
-  get(){
-    return props.modelValue
-  },
-  set(isChecked){
-    emit('update:modelValue', isChecked)
-  }
-})
-
-interface Props {
-  modelValue: boolean;
-}
-
-const props = defineProps<Props>();
-
-const emit = defineEmits<{
-  'update:modelValue': [value: boolean]
-}>()
-
+const emit = defineEmits(['action'])
 </script>
-
-<style scoped></style>
