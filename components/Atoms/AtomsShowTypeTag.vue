0b3483a626ea623d6c3618534556375726ce886d
Alan Lima <dev.alanlima@gmail.com>
2023-12-12 13:53:34 -0300

    feat: Create tag for show type

---
+++ components/Atoms/AtomsShowTypeTag.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="bg-color_light h-[32px] px-3 grid place-items-center rounded-full text-color_highlight font-semibold"
+    :title="props.showType === 'movies' ? 'Movie' : props.showType === 'tv' ? 'Tv | Serie' : 'All'"
+  >
+    {{ props.showType === 'movies' ? "Movie" : props.showType === "tv" ? "Tv | Serie" : "All" }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  showType: ShowType;
+}
+
+const props = defineProps<Props>();
+</script>
