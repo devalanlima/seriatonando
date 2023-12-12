13081b1c401a92034af64605dcdaa4d623966d96
Alan Lima <dev.alanlima@gmail.com>
2023-12-12 10:58:08 -0300

    feat: Add index prop

--- components/Atoms/AtomsBackgroundBlur.vue
+++ components/Atoms/AtomsBackgroundBlur.vue
@@ -1,5 +1,11 @@
 <template>
   <teleport to="body">
-    <div class="fixed w-screen h-screen bg-color_dark/70 backdrop-blur-sm top-0 left-0 z-40"></div>
+    <div :class="['fixed w-screen h-screen bg-color_dark/70 backdrop-blur-sm top-0 left-0', props.index ]"></div>
   </teleport>
 </template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{index: string}>(),{
+  index: 'z-40'
+});
+</script>
