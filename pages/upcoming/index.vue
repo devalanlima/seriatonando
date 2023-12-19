70c84a782335ce67a0a0466c0a2baf12bc1d6f45
Alan Lima <dev.alanlima@gmail.com>
2023-12-19 10:40:51 -0300

    feat: Create upcoming page

--- pages/upcoming/index.vue
+++ pages/upcoming/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <h2>Up coming</h2>
+  <OrganismsShowsGridLayout
+    :movie-params="movieParams"
+    :tv-params="tvParams"
+    url-api-movie="/api/TMDBDiscoverMovie"
+    url-api-tv="/api/TMDBDiscoverTv"
+  />
 </template>
 
 <script setup lang="ts">
+const projectStore = useProjectStore();
+const tmdbFiltersStore = useTMDBFiltersStore();
 
+const movieParams = computed(() => {
+  return {
+    "language": projectStore.language,
+    "watch_region": projectStore.region,
+    "primary_release_date.gte": tmdbFiltersStore.releaseDateLte,
+
+  };
+});
+
+const tvParams = computed(() => {
+  return {
+    "language": projectStore.language,
+    "first_air_date.gte": tmdbFiltersStore.releaseDateLte,
+    "watch_region": projectStore.region,
+  };
+});
 </script>
